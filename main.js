955fa298280ad91757bd2937c8f1d3abe4386c42
Alok Ranjan Pandit <alokranjanpandit005.com>
2024-05-04 13:38:46 +0530

    To check jenkins test

--- main.js
+++ main.js
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 3000;
 app.use("/restaurent",restaurent);
 
 app.listen(PORT , ()=>{
-    console.log(`Server running on Port ${PORT}`)
+    console.log(`Server running on Port ${PORT}`);
 })
