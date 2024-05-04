0e137ab3772613f4f10dc8c4025f02396dc0c9d6
Alok Ranjan Pandit <alokranjanpandit005.com>
2024-05-04 13:20:48 +0530

    Repush

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const restaurent = require("./routes/restaurent");
 mongoConn("mongodb://127.0.0.1:27017/restaurent_details")
 const PORT = process.env.PORT || 3000;
 
-app.use("/restaurent",restaurent)
+app.use("/restaurent",restaurent);
 
 app.listen(PORT , ()=>{
     console.log(`Server running on Port ${PORT}`)
