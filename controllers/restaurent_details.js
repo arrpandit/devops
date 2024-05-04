d2ad7a4e0aecba546bd5d113d160bb083e239884
Alok Ranjan Pandit <alokranjanpandit005.com>
2024-05-04 15:17:05 +0530

    jenkins test 05

--- controllers/restaurent_details.js
+++ controllers/restaurent_details.js
@@ -1,4 +1,4 @@
-const {Address, UserReview, Restaurant} = require("../models/restaurent")
+const {Address, UserReview, Restaurant} = require("../models/restaurent");
 
 async function allrestro(req,res){
     const allrestaurent = await Restaurant.find({});
